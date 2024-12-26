2a75a3b7ac735de3d30c7370185268ec1ac037fa
susigan <susigan@gmail.com>
2024-12-26 21:25:35 +0000

    tesdte1

--- README.md
+++ README.md
@@ -1,19 +1,4 @@
-# 🎈 Blank app template
+import streamlit as st
 
-A simple Streamlit app template for you to modify!
-
-[![Open in Streamlit](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://blank-app-template.streamlit.app/)
-
-### How to run it on your own machine
-
-1. Install the requirements
-
-   ```
-   $ pip install -r requirements.txt
-   ```
-
-2. Run the app
-
-   ```
-   $ streamlit run streamlit_app.py
-   ```
+st.set_page_config(page_title="Análise de Dados de Atividades", layout="wide")
+st.title("Dashboard de Treinamento")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,31 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(page_title="Análise de Dados de Atividades", layout="wide")
+st.title("Dashboard de Treinamento")
+
+
+import pandas as pd
+
+@st.cache
+def load_data(sheet_name):
+    # Exemplo com pandas e Google Sheets API
+    url = "https://docs.google.com/spreadsheets/d/10pefcY6VI4Z45M8Y69D6JxIoqOkjzSlSpV1PMLXoYlI/edit?gid=0#gid=0A"
+    return pd.read_excel(url, sheet_name=sheet_name)
+
+import matplotlib.pyplot as plt
+
+def plot_activity(data):
+    plt.figure(figsize=(10, 6))
+    plt.plot(data['Data'], data['RPE'])
+    plt.title("RPE ao longo do tempo")
+    plt.xlabel("Data")
+    plt.ylabel("RPE")
+    st.pyplot(plt)
+
+def calculate_effort(data):
+    data["Effort"] = data["RPE"] * data["Tempo(min)"]
+    return data
+
+selected_years = st.multiselect("Selecione os anos:", [2022, 2023, 2024, 2025])
+filtered_data = data[data["Data"].dt.year.isin(selected_years)]
+
